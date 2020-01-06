ba54d9153f0aeb7f7ca1eef252f16de56bf47046
Hamed Momeni <hamed.ma7@gmail.com>
2020-01-06 21:54:16 +0330

    added ShippingTime bare classes

---
+++ database/factories/ShippingTimeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\ShippingTime;
+use Faker\Generator as Faker;
+
+$factory->define(ShippingTime::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
