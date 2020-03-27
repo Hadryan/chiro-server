1f7e8df016d7b39db7affcc2499d8add1d3723a5
Hamed Momeni <hamed.ma7@gmail.com>
2020-03-27 17:45:38 +0430

    added icon_id to shipping address

--- app/Model/ShippingAddress.php
+++ app/Model/ShippingAddress.php
@@ -6,13 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class ShippingAddress extends Model
 {
-    protected $fillable = ['name', 'customer_id', 'city_id', 'lat', 'lng', 'address'];
+    protected $fillable = ['name', 'customer_id', 'city_id', 'lat', 'lng', 'address', 'icon_id'];
 
     public $timestamps =  false;
 
     protected $casts = [
         'lat' => 'float',
         'lng' => 'float',
+        'icon_id' => 'integer'
     ];
 
     public function user()
